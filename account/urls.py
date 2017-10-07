576c4f2d50fd116abd00204918fcb45d4c263e30
aryanappiness <aryan@aappinessworld.com>
2017-10-07 18:31:12 +0530

    add new app

--- account/urls.py
+++ account/urls.py
@@ -4,6 +4,7 @@ from .views import *
 
 urlpatterns = [
     url(r'^signup/$', SignUpView.as_view({'post': 'register'})),
+    url(r'^login/$', Login.as_view({'post': 'post'})),
     url(r'^change/password/$', ChangePasswordView.as_view({'post': 'post'})),
     url(r'^forget/password/$', ResetPasswordView.as_view({'post': 'reset'})),
     url(r'^forget/password/done/$', ResetPasswordView.as_view({'post': 'reset_done'})),
